f564764cbfd96d52fd3cd24887047026b316bf33
Jake Pittis <jake.pittis@shopify.com>
2018-03-01 14:22:35 -0500

    pod parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,29 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Kube
+import Objects
+
+import Data.Aeson (decodeStrict, ToJSON, FromJSON)
+import Turtle
+import Data.Text (Text)
+import Data.Text.Encoding (encodeUtf8)
+import GHC.Generics
+
+-- Used to decode JSON list of pods.
+newtype PodSet = PodSet { items :: [Pod] }
+  deriving (Show, Eq, Generic, FromJSON)
+
+parser :: Parser Text
+parser = optText "context" 'c' "the context to visualize"
+
+-- A WIP main for messing around with parsing.
 main :: IO ()
-main = return ()
+main = do
+  context  <- options "kubepacity" parser
+  names    <- getNodeNames context
+  podsJSON <- strict . getPods context . head $ names
+  let (Just set) = decodeStrict . encodeUtf8 $ podsJSON
+  mapM_ print (items set)
--- src/Objects.hs
+++ src/Objects.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 module Objects
         ( Pod(..)
         , Container(..)
         ) where
 
 import Data.Text (Text)
+import Data.Aeson
+import Control.Monad (forM)
+import Control.Applicative (optional)
 
 data Pod = Pod
   { name       :: Text
@@ -12,6 +17,35 @@ data Pod = Pod
 
 data Container = Container
   { cname      :: Text
-  , cpuRequest :: Text
-  , memRequest :: Text
+  , cpuRequest :: Maybe Text
+  , memRequest :: Maybe Text
   } deriving (Show, Eq)
+
+instance FromJSON Pod where
+  parseJSON = withObject "pod" $ \o -> do
+    metadata <- o        .: "metadata"
+    name     <- metadata .: "name"
+
+    spec       <- o    .: "spec"
+    containers <- spec .: "containers"
+
+    cnames <- forM containers $ \container -> do
+      cname      <- container .: "name"
+
+      cpuRequest <- optional (container .: "resources") >>= \case
+        Nothing        -> return Nothing
+        Just resources ->
+          optional (resources .: "requests") >>= \case
+            Nothing       -> return Nothing
+            Just requests -> optional (requests .: "cpu")
+
+      memoryRequest <- optional (container .: "resources") >>= \case
+        Nothing        -> return Nothing
+        Just resources ->
+          optional (resources .: "requests") >>= \case
+            Nothing       -> return Nothing
+            Just requests -> optional (requests .: "memory")
+
+      return (Container cname cpuRequest memoryRequest)
+
+    return (Pod name cnames)
